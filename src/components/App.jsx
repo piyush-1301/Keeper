c45a8f7bc409daf1a0b2b67a5763b04943453b9a
Piyush Soni <piyushsoni0069@gmail.com>
2020-11-25 12:20:53 +0530

    keeper full

---
+++ src/components/App.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+import axios from "axios";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+  useEffect(() => {
+    axios.get("/note").then((res) => {
+      setNotes(res.data);
+    });
+  }, []);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+
+    axios
+      .post("/note", { t: newNote.title, c: newNote.content })
+      .then(function (response) {
+        console.log(response.data);
+      });
+  }
+
+  function deleteNote(id) {
+  //  notes.forEach((currentValue,index) => {
+  //    if(index===id){
+  //     axios.delete("/note/"+currentValue.title).then((res)=>{
+  //       setNotes(res.data);
+  //     })
+  //    }
+  //  });
+  
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        if(index===id){
+          axios.delete("/note/"+noteItem.title).then((res)=>{
+            console.log(res.data);
+          })
+        }
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
