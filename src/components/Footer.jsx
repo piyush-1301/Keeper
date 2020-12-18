c473b649a13890e29c0a0f8b89f0fbc9e1a73699
Piyush Soni <piyushsoni0069@gmail.com>
2020-12-18 06:37:20 +0530

    google adsense

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ⓒ {year}</p>
-    </footer>
+    <p>Copyright ⓒ {year}</p>
+  </footer>
   );
 }
 
 export default Footer;
+
+
+
+
+
+// <div class="card-footer text-muted" style={{display:"block",bottom:"0"}}>
+// Copyright ⓒ {year}
+// </div>
